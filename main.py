5592e1404f0d5111e1bc3621b9ebc0e790c49b9d
TSOT7 <tommyma1234@gmail.com>
2021-07-09 23:22:19 -0400

    Initial Main Script

---
+++ main.py
@@ -0,0 +1,9 @@
+# Welcome to the code for cottage-tui!
+# The main program will be placed here 
+
+#imports
+# https://stackoverflow.com/questions/17211188/how-to-create-a-timer-on-python
+#import npyscreen
+#import simpleaudio
+
+
